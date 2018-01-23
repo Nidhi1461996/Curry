6e79b5b11c7d8c772d55a2a9e30266ec30d28529
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-23 18:18:23 +0530

    added test cases for invalid parameters

--- Binary.js
+++ Binary.js
@@ -1,5 +1,16 @@
 function binary(inputOne, inputTwo){
+
+  if (inputOne===undefined || inputTwo===undefined){
+    return undefined;
+  }
+
+  if(isNaN(inputOne) || isNaN(inputTwo)) {
+    return null;
+  }
+
   return inputOne+inputTwo;
 }
+
+
 
 module.exports=binary;
--- Binary.test.js
+++ Binary.test.js
@@ -1,5 +1,7 @@
 const binary = require('./Binary');
 
+
+//valid parameters
 describe('with valid inputs', () => {
   test('With positive numbers', () => {
     expect(binary(4,5)).toBe(9);
@@ -11,5 +13,30 @@ describe('with valid inputs', () => {
 
   test('With positive and negative numbers', () => {
     expect(binary(-4,5)).toBe(1);
+  });
+});
+
+
+//invalid parameters
+describe('testing with invalid parameters', () => {
+
+  test('With no input', () => {
+    expect(binary()).toBe(undefined);
+  });
+
+  test('With one parameter as undefined', () => {
+    expect(binary(undefined,5)).toBe(undefined);
+  });
+
+  test('With both parameters as undefined', () => {
+    expect(binary(undefined,undefined)).toBe(undefined);
+  });
+
+  test('With NaN as a parameter', () => {
+    expect(binary(NaN,5)).toBe(null);
+  });
+
+  test('With NaN as a parameter', () => {
+    expect(binary(NaN,NaN)).toBe(null);
   });
 });
