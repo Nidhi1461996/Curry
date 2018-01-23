bf2419ba1da6153b424298745197eb344a897208
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-23 18:32:49 +0530

    tested with positive and  negative numbers

--- delayInvocation.test.js
+++ delayInvocation.test.js
@@ -8,6 +8,10 @@ describe('Valid inputs', () => {
   });
 
   test('negative numbers as arguments', () =>{
+    expect(check(-1)(-2)).toBe(-3);
+  });
+
+  test('With positive and negative numbers', () =>{
     expect(check(-1)(2)).toBe(1);
   });
 
